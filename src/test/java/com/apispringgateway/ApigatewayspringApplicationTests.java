7eb219119ef79a504b6a24f50aecb23812e4aaed
koteshchevula19 <koteschevula11@gmail.com>
2024-02-15 12:42:42 +0530

    added src and pom

---
+++ src/test/java/com/apispringgateway/ApigatewayspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apispringgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
